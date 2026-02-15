cfaa286829b1fd31e1603d1cf7309c21f02b6fc0
kathigg <kathigg@udel.edu>
2026-02-15 10:27:14 -0500

    solved

--- README.md
+++ README.md
@@ -3,3 +3,9 @@
 Hello! This repository has been pre-configured with eslint, prettier, and a github actions workflow to automatically lint and format your code on every push. It'll also deploy your site for you.
 
 You'll complete programming problems by merging in upstream branches. Check out the textbook for more information: <https://frontend-fun.github.io/react-hooks-typescript-tome/>
+
+Note to self: to push to a specific branch when finished with a task, use:
+
+```
+git push --set-upstream origin solved-html-css
+```
--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
 import React from "react";
 import "./App.css";
+import { Button, Col, Container, Row } from "react-bootstrap";
+import "react-bootstrap";
+
+import themeTeaser from "./assets/Copy of Theme Teasers (1).png";
 
 function App(): React.JSX.Element {
     return (
@@ -11,6 +15,48 @@ function App(): React.JSX.Element {
                 Hello World Edit <code>src/App.tsx</code> and save. This page
                 will automatically reload.
             </p>
+            <h1>Hello!</h1>
+            <Container>
+                <Row>
+                    <Col>
+                        <img
+                            src={themeTeaser}
+                            style={{ width: "270px", height: "337px" }}
+                            alt="HenHacks is only two weeks away!"
+                        />
+                    </Col>
+                    <Col>
+                        <p>{"Things I'm eating for food today:"}</p>
+                        <ul>
+                            <li>{"Strawberries"}</li>
+                            <li>{"Raspberries"}</li>
+                            <li>{"1 Mango"}</li>
+                            <li>{"Tapenade and Garlic Tuscan Bread"}</li>
+                            <li>{"Trader Joe's Fries"}</li>
+                            <li>{"Perhaps Spring Rolls"}</li>
+                            <li>{"Mochi"}</li>
+                            <li>
+                                {"Some chocolate covered frozen strawberries"}
+                            </li>
+                        </ul>
+                        <Button
+                            onClick={() => {
+                                console.log("Hello World!");
+                            }}
+                        >
+                            Log Hello World
+                        </Button>
+                        <div
+                            id="rectangle"
+                            style={{
+                                width: "10px",
+                                height: "10px",
+                                backgroundColor: "red",
+                            }}
+                        ></div>
+                    </Col>
+                </Row>
+            </Container>
         </div>
     );
 }
