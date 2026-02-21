573f51a338e9d52b775e10447468d818da2a2efa
kathigg <kathigg@udel.edu>
2026-02-21 11:36:47 -0500

    updated to try and fix linting errors

--- src/functions.ts
+++ src/functions.ts
@@ -1,4 +1,4 @@
-import { findAllByTestId } from "@testing-library/dom";
+// import { findAllByTestId } from "@testing-library/dom";
 
 /**
  * Consumes a single temperature in Fahrenheit (a number) and converts to Celsius
