b27af3d0ad9f86afcad24a07cb695bba57ebd573
kathigg <kathigg@udel.edu>
2026-03-08 14:01:05 -0400

    done with another one

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import "./App.css";
 // import { ChangeType } from "./components/ChangeType";
-// import { RevealAnswer } from "./components/RevealAnswer";
-// import { StartAttempt } from "./components/StartAttempt";
+import { RevealAnswer } from "./components/RevealAnswer";
+import { StartAttempt } from "./components/StartAttempt";
 // import { TwoDice } from "./components/TwoDice";
 // import { CycleHoliday } from "./components/CycleHoliday";
 import { Counter } from "./components/Counter";
@@ -16,12 +16,13 @@ function App(): React.JSX.Element {
             <hr></hr>
             <Counter></Counter>
             <hr />
-            {/* <RevealAnswer></RevealAnswer>
+            <RevealAnswer></RevealAnswer>
             <hr />
             <StartAttempt></StartAttempt>
             <hr />
             <TwoDice></TwoDice>
             <hr />
+            {/*
             <ChangeType></ChangeType>
             <hr />
             <CycleHoliday></CycleHoliday> */}
