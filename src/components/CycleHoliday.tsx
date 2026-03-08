a6516faea136d2d4dfd3be272b2fad259da39dee
kathigg <kathigg@udel.edu>
2026-03-08 15:12:12 -0400

    should be completed!

--- src/components/CycleHoliday.tsx
+++ src/components/CycleHoliday.tsx
@@ -2,19 +2,41 @@ import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
 export function CycleHoliday(): React.JSX.Element {
-    const [holiday, useHoliday] = useState<string>("St. Lucia's Day");
-
-    function alphabetHolidayOrder(): string {
-        return "";
+    const [holiday, setHoliday] = useState<string>("St. Lucia's Day 🕯️");
+    // saint hans day is summer, st lucias is christmas, fastelavn is february
+    const holidays: string[] = [
+        "St. Lucia's Day 🕯️",
+        "Saint Hans Dag 🔥",
+        "Fastelavn 👯‍♀️",
+        "Påskedag 🐣",
+        "Mortensaften 🦆",
+    ];
+    function dateHolidayOrder(): void {
+        setHoliday(
+            holiday === "St. Lucia's Day 🕯️" ? "Fastelavn 👯‍♀️"
+            : holiday === "Fastelavn 👯‍♀️" ? "Påskedag 🐣"
+            : holiday === "Påskedag 🐣" ? "Saint Hans Dag 🔥"
+            : holiday === "Saint Hans Dag 🔥" ? "Mortensaften 🦆"
+            : holiday === "Mortensaften 🦆" ? "St. Lucia's Day 🕯️"
+            : "Saint Hans Dag 🔥",
+        );
     }
 
-    function dateHolidayOrder(): string {
-        return "";
+    function alphabetHolidayOrder(): void {
+        setHoliday(
+            holiday === "St. Lucia's Day 🕯️" ? "Fastelavn 👯‍♀️"
+            : holiday === "Fastelavn 👯‍♀️" ? "Mortensaften 🦆"
+            : holiday === "Mortensaften 🦆" ? "Påskedag 🐣"
+            : holiday === "Påskedag 🐣" ? "Saint Hans Dag 🔥"
+            : holiday === "Saint Hans Dag 🔥" ? "St. Lucia's Day 🕯️"
+            : "Fastelavn 👯‍♀️",
+        );
     }
     return (
         <div>
-            <Button>Cycle Alphabetically</Button>
-            <Button>Cycle by Time of Year</Button>
+            <Button onClick={alphabetHolidayOrder}>Cycle Alphabetically</Button>
+            <Button onClick={dateHolidayOrder}>Cycle by Time of Year</Button>
+            <span>{"Holiday: " + holiday}</span>
         </div>
     );
 }
